7eec33dbe321ccb753dd8f26252406627c9f7025
kundan7783 <kundan065335@gmail.com>
2026-02-03 15:25:21 +0530

    change database port

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ const myDB = mysql.createPool({
     user : process.env.DB_USER,
     password : process.env.DB_PASS,
     database : process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections : true,  
     connectionLimit : 10,    
     queueLimit : 0,         
