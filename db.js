4a65dbe8a831eb21caa5fa3d31552b9a7e68b942
kundan7783 <kundan065335@gmail.com>
2026-02-03 13:55:32 +0530

    change databse name live

--- db.js
+++ db.js
@@ -5,7 +5,11 @@ const myDB = mysql.createPool({
     user : process.env.DB_USER,
     password : process.env.DB_PASS,
     database : process.env.DB_NAME,
-    port : process.env.DB_PORT
+    waitForConnections : true,  
+    connectionLimit : 10,    
+    queueLimit : 0,         
+    connectTimeout: 20000, 
+    acquireTimeout: 20000  
 });
 
 async function getCurrentDatabase() {
